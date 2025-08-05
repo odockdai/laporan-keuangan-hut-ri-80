75953c9247585d60e0a39813b39b58184b0675d0
irfan <irfanoktora.financial@gmail.com>
2025-08-06 05:49:30 +0700

    add link gdrive

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**', // Mengubah ini menjadi lebih permisif
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
