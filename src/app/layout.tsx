a064b93c2fd29b7295b0b5e36f1640faf635a6d4
irfan <irfanoktora.financial@gmail.com>
2025-08-06 07:00:19 +0700

    admit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
-
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google"; // Dihapus karena tidak digunakan
+// import { Geist, Geist_Mono } from "next/font/google"; // Dihapus sepenuhnya
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "KEDEMPEL",
