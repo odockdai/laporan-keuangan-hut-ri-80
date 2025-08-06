452f77f685d300fe886c57b1ef54b0053ecdff8a
irfan <irfanoktora.financial@gmail.com>
2025-08-06 07:39:16 +0700

    new pallete and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google"; // Dihapus sepenuhnya
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "KEDEMPEL",
@@ -14,8 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className="dark">
-      <body className="antialiased">
-        {children}
+      <body className="flex flex-col min-h-screen antialiased">
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
