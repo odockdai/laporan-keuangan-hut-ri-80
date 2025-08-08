502de600068703c1dd06ed840d64832f1d276263
irfan <irfanoktora.financial@gmail.com>
2025-08-09 01:58:40 +0700

    fix this

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 import { createClient } from 'redis';
 import Papa from 'papaparse';
 
